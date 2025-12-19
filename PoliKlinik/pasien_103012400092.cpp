1c7d3ec31a1ecf791cfb891c111606c5cd94c443
JonathanGilbert17 <jonathan.tjahja17@gmail.com>
2025-12-20 00:07:05 +0700

    Update pasien_103012400092.cpp

--- PoliKlinik/pasien_103012400092.cpp
+++ PoliKlinik/pasien_103012400092.cpp
@@ -1,1 +1,22 @@
 #include "pasien.h"
+
+addressP alokasiPasien(infotypeP data) {
+    addressP P = new elmPasien;
+    P->info = data;
+    P->next = nullptr;
+    P->prev = nullptr;
+    return P;
+}
+
+void insertLastPasien(addressP &first, addressP P) {
+    if (first == nullptr) {
+        first = P;
+    } else {
+        addressP last = first;
+        while (last->next != nullptr) {
+            last = last->next;
+        }
+        last->next = P;
+        P->prev = last;
+    }
+}
