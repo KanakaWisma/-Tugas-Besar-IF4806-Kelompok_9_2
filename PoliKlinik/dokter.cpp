36958a78788e2e33bbad9dfddc8adace70517965
KanakaWisma <wismaarr@gmail.com>
2025-12-19 18:43:35 +0700

    Update dokter.cpp
    Penambahan Fitur Case 4 dan 5

--- PoliKlinik/dokter.cpp
+++ PoliKlinik/dokter.cpp
@@ -151,3 +151,67 @@ void cariRiwayatPasien(ListDokter L, string idPasien) {
         cout << "Pasien ini belum pernah berobat ke dokter manapun.\n";
     }
 }
+
+// Komputasi ke-2: Mencari dokter dengan pasien terbanyak
+void dokterTerpopuler(ListDokter L) {
+    addressD P = L.first;
+    if (P == nullptr) {
+        cout << "Data dokter kosong.\n";
+        return;
+    }
+
+    addressD maxDok = P;
+    int maxCount = hitungJumlahPasien(P);
+    P = P->next;
+
+    while (P != nullptr) {
+        int currentCount = hitungJumlahPasien(P);
+        if (currentCount > maxCount) {
+            maxCount = currentCount;
+            maxDok = P;
+        }
+        P = P->next;
+    }
+
+    cout << "Dokter paling populer: " << maxDok->info.namaDokter
+         << " dengan " << maxCount << " pasien.\n";
+}
+
+// Menampilkan semua child (pasien) secara unik berdasarkan ID
+void printSemuaPasienUnik(ListDokter L) {
+    cout << "--- Daftar Semua Pasien di Rumah Sakit (Unik) ---\n";
+    addressD D = L.first;
+    bool found = false;
+
+    // buat ngecek unik, kita bandingin ID pasien yang sekarang
+    // sama semua pasien yang udah muncul sebelumnya
+    while (D != nullptr) {
+        addressP P = D->firstChild;
+        while (P != nullptr) {
+            // Cekk, ID ini sudah pernah muncul di dokter-dokter sebelumnya apa belon
+            bool sudahAda = false;
+            addressD prevD = L.first;
+            while (prevD != D && !sudahAda) {
+                addressP prevP = prevD->firstChild;
+                while (prevP != nullptr) {
+                    if (prevP->info.idPasien == P->info.idPasien) {
+                        sudahAda = true;
+                        break;
+                    }
+                    prevP = prevP->next;
+                }
+                prevD = prevD->next;
+            }
+
+            // Kalo belum pernah muncul, baru print gezt
+            if (!sudahAda) {
+                cout << "- ID: " << P->info.idPasien << " | Nama: " << P->info.namaPasien << endl;
+                found = true;
+            }
+            P = P->next;
+        }
+        D = D->next;
+    }
+
+    if (!found) cout << "(Belum ada data pasien)\n";
+}    
